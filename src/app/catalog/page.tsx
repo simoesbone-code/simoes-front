1e222ff8e331adaaf603520f23d5ab90409edb66
blogedson5-ux <blogedson5@gmail.com>
2026-04-09 21:56:36 -0300

    Atualizacao de categoria

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,9 +1,9 @@
 "use client";
+
 import Catalog from "@/components/CatalogPage";
 import { getProduct } from "@/hooks/useClient";
 import { PropsProduct } from "@/types/product";
 import { useQuery } from "@tanstack/react-query";
-import { Wrapper } from "../dashboard/admin/[id]/styles";
 
 export default function CatalogPage() {
   const { data: dataProduct, refetch } = useQuery<PropsProduct[]>({
@@ -12,8 +12,8 @@ export default function CatalogPage() {
   });
 
   return (
-    <Wrapper>
+    <div className="w-full max-w-full overflow-x-hidden">
       <Catalog refetch={refetch} products={dataProduct} />
-    </Wrapper>
+    </div>
   );
 }
